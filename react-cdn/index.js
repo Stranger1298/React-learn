4211bdee9a53c50787a46779419a3b40b0371099
Aman <amanraj89969@gmail.com>
2024-05-19 16:05:25 +0530

    created a plain navbar elements

--- react-cdn/index.js
+++ react-cdn/index.js
@@ -2,22 +2,38 @@
 
 // ReactDOM.render(<h1>Hello everyone</h1>,document.getElementById('root'))
 
-let i=0;
-function add(){
-    i++
-    console.log(i);
-    rendercontent()
-}
-const display=document.getElementById('root')
+// let i=0;
+// function add(){
+//     i++
+//     console.log(i);
+//     rendercontent()
+// }
+// const display=document.getElementById('root')
 
-const rendercontent=()=>{
+// const rendercontent=()=>{
+//     let content=(
+//         <div>
+//         <button id="btn" onClick={add}>Tap here!!</button>
+//         <h1>{i}</h1>
+//         </div>
+//         )
+//         ReactDOM.render(content,display)
+// }
+
+// rendercontent()
+
+//Navbar
+const display = document.getElementById('root')
+
     let content=(
         <div>
-        <button id="btn" onClick={add}>Tap here!!</button>
-        <h1>{i}</h1>
+        <h1>Brand</h1>
+        <ul>
+            <li>Home</li>
+            <li>Contact</li>
+            <li>Pricing</li>
+        </ul>
+        
         </div>
         )
         ReactDOM.render(content,display)
-}
-
-rendercontent()
